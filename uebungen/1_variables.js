4d42599c6e2d4f861b0b9187045206d6506ec4e2
joshua <joshua.schreiber.bg@bs-gn.de>
2024-05-09 11:45:58 +0200

    Changes to VariablesÜbung, Types Übung erledigt

--- uebungen/1_variables.js
+++ uebungen/1_variables.js
@@ -6,6 +6,9 @@ console.log(message)
 
 const constanteVariable = "Nicht veränderbar"
 
+// Erstelle eine konstante Variable name welche deinen Namen enthält
+// Erstelle eine leere Variable awesomePerson und befülle sie mit dem Wert der name Variable
+// Gib den Inhalt von awesomePerson in der Konsole aus
 const name = "Joshua Schreier"
 let awsomePerson
 awsomePerson = name
---
+++ uebungen/2_types.js
@@ -0,0 +1,28 @@
+const name = "Haki"
+const greeting = "Hello dear Human"
+
+const pi = 3.141
+let currentYear = 2024
+
+const dogsAreFluffy = true
+const pigsCanFly = false
+
+let meainingOfExistence = "Still undefined"
+console.log(meainingOfExistence)
+
+/* 
+Legt eine Variable age an, welche euer Alter in ganzen Jahren enthält
+Gib den Inhalt der age Variable in der Konsole aus
+Legt eine Variable hasPet an, welche enthält, ob ihr ein Haustier habt, oder nicht
+Gib den Inhalt der hasPet Variable in der Konsole aus
+Legt eine Variable sibling an
+Falls du Geschwister hast, befüll die Variable sibling mit dem Namen eines deiner Geschwister
+Gib den Inhalt der sigbling Variable in der Konsole aus
+*/
+
+let age = 18
+console.log(age)
+let hasPet = false
+console.log(hasPet)
+let sibling = "Theres, Lukas"
+console.log(sibling)
