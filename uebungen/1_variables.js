65d3bde279fdc9e216ced4b928316fbda3505225
joshua <joshua.schreiber.bg@bs-gn.de>
2024-05-09 11:31:44 +0200

    Variablen Aufgabe erledigt

---
+++ uebungen/1_variables.js
@@ -0,0 +1,12 @@
+let message = "Hallo"
+let oldMessage = message
+message = "Hallo, du da!"
+console.log(oldMessage)
+console.log(message)
+
+const constanteVariable = "Nicht veränderbar"
+
+const name = "Joshua Schreier"
+let awsomePerson
+awsomePerson = name
+console.log(awsomePerson)
